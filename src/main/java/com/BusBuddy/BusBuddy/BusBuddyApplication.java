82fa34aaa8b4ccdcd5c77c1b9f170c3828f5468c
RaninduAmarasinghe <raninduamarasinghe@gmail.com>
2024-12-20 23:36:50 +0530

    first commit

---
+++ src/main/java/com/BusBuddy/BusBuddy/BusBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.BusBuddy.BusBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusBuddyApplication.class, args);
+	}
+
+}
